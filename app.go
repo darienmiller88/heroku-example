4945168f0df2c54c4ec142a61c34ec3edadcb601
darienmiller88 <darienmiller88@yahoo.com>
2020-03-31 21:06:01 -0400

    change

--- app.go
+++ app.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello")	
+	fmt.Println("Server is running")
 
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":5000", nil)
+
+}
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "<h1>server is up</h1>")
 }
